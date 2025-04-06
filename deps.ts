c7a6eac58b14a226a7e27907cf6efbf57c881c6e
Andrija Petrovic <andrija.hers@gmail.com>
2025-04-06 12:56:11 +0200

    Init

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export * as pg from 'npm:pg';
+export type {Client as ClientType} from 'npm:pg';
---
+++ mod.ts
@@ -0,0 +1,83 @@
+import pg from 'npm:pg';
+const {Client} = pg;
+type ClientType = typeof Client;
+import {IQueryable, IQueryRows, IQueryResult, IQueryResultAlike} from '../sql-executor/mod.ts';
+import {AllexEvent} from "https://raw.githubusercontent.com/ts-libs/lib/refs/heads/main/event.ts";
+
+
+export interface IPgConfiguration {
+  host: string;
+  user: string;
+  password: string;
+  database: string;
+}
+
+interface Result {
+  rows: IQueryRows;
+  rowCount: number;
+  affectedRows:number;
+}
+
+function pgQueryResult(result: Result) : IQueryResult {
+  const retVal = result.rows as IQueryResultAlike;
+  retVal.affectedRows = result.rowCount;
+  return retVal;
+}
+
+export class PgQueryable implements IQueryable{
+  private config: IPgConfiguration;
+  private client: ClientType;
+  private isConnected: boolean;
+  public disconnected: AllexEvent<void> = new AllexEvent<void>;
+  constructor(config: IPgConfiguration) {
+    this.config = config;
+    this.doClient();
+    this.isConnected = false;
+  }
+  get statementDelimiter(): string {
+    return ';\r';
+  }
+  get connected(): boolean {
+    return this.client?._connected;
+  }
+  async connect(): Promise<boolean> {
+    if (this.connected) {
+      return true;
+    }
+    try {
+      await this.client.connect();
+      return true;
+    } catch(e) {
+      console.error(e);
+      this.doClient();
+      return false;
+    }
+  }
+  disconnect(): Promise<void> {
+    this.client?.end();
+    return Promise.resolve();
+  }
+  async query(queryString: string): Promise<Array<IQueryResult>> {
+    if (!this.connected) {
+      throw new Error('Maybe we should not raise an error here, but what else?');
+    }
+    const results: Result|Array<Result> = await this.client.query(queryString);
+    if (!results) {
+      return [];
+    }
+    if (!Array.isArray(results)) {
+      return [pgQueryResult(results)];
+    }
+    return results.map(r => pgQueryResult(r));
+  }
+
+  private doClient () {
+    this.client = new Client(this.config);
+    this.client.on('end', () => {
+      this.isConnected = false;
+      this.disconnected.fire();
+    });
+  }
+}
+
+
